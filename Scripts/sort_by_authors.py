f84362da67be865587d44e105d9720d1db40e7f5
LeRagib <gabriel.vaubaillon@gmail.com>
2023-04-06 22:13:15 +0100

    Changed sort, created objects to play with the library

--- Scripts/sort_by_authors.py
+++ Scripts/sort_by_authors.py
@@ -1,58 +1,11 @@
-# A data-line in the file is built this way :
-# | Titre | Auteur(s) | Langue | ISBN | Possédé | Lu | Situation | Notes |
+import data_management as dm
 
+if __name__ == "__main__":
+    filename = "../possedes_ou_lus.md"
+    with open(filename, "r") as f:
+        library = dm.list_from_md_file(f)
+    library.sort()
 
-# Open the file, extract all the data
-# -----------------------------------
-f = open("possedes_ou_lus.md", "r")
-lignes = f.readlines()
-f.close()
-
-header = lignes[:2]
-# la derniere ligne est vide
-data = [ l[:-1].split("|") for l in lignes[2:-1]]
-
-# Sort books by authors. By the LASTNAME
-
-def sorting_key(d):
-    # Le nom de l'auteur
-    tmp = d[2]
-
-    # Si deux auteurs, on garde le premier
-    if "&" in tmp:
-        tmp = (tmp.split("&"))[0]
-
-    # On enleve les espaces autours du nom
-    tmp = tmp.strip(" ")
-
-    # Les bouquins sans auteurs : à la fin
-    if tmp == "":
-        return "Zzzz"
-
-    # On separe le prénom et le nom
-    tmp = tmp.split(" ")
-
-    # On garde le nom
-    res = tmp[-1]
-
-    # On enleve le fils/ aîné (Alexandre Dumas Fils sera classé à Dumas)
-    if res.lower() == "fils" or res.lower() == "père" or res.lower() == "aîné" or res.lower() == "junior" or res.lower() == "senior":
-        res = tmp[-2]
-    return res
-
-data = sorted(data, key=lambda x: sorting_key(x))
-
-
-
-# Once data is sorted, rewrite it
-f = open("sorted", "w")
-f.write(header[0])
-f.write(header[1])
-for d in data:
-    ch = ""
-    for st in d[1:]:
-        ch += "|"
-        ch += st
-    ch += "\n"
-    f.write(ch)
-f.close()
+    print(dm.HEADER, end="")
+    for book in library:
+        print(book.to_str())
